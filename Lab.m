aa53e81b5bf8e79250e46c7e2a7c63442e851158
glesssard <lesg2605@usherbrooke.ca>
2019-10-16 09:25:36 -0400

    Ex 1 done

--- Lab.m
+++ Lab.m
@@ -4,4 +4,23 @@ clc
 close all
 
 %% Ex 1
-N = 10;
+N = 100;
+x_barre = [0 0 0];
+s = [0 0 0];
+r = 0;
+
+mu = mean(r);
+sigma = 10/sqrt(12);
+
+for n=[100 1000 10000]
+    r = -5 + (5+5)*rand(n,1);
+    x_barre = mean(r);
+    s = std(r);
+
+
+    figure
+    histogram(r)
+end
+
+%% Ex 2
+
